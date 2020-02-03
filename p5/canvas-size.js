3404e9076676d4f584e05c760c971523e20d80e0
yusakuhamajima <yusakuhamajima@gmail.com>
2020-02-03 21:28:03 +0900

    color change

--- p5/canvas-size.js
+++ p5/canvas-size.js
@@ -1,8 +1,12 @@
 function setup() {
   var canvas = createCanvas(320, 480, P2D);
   canvas.parent("canvas");
+  colorMode(HSB, 360, 100, 100, 100);
 }
 
 function draw() {
-  background(0);
+  background(270, 100, 100, 100);
+  noStroke();
+  fill(90, 100, 100, 100);
+  rect(100, 100, 100, 100);
 }
