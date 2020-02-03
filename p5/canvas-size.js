77b67c96851bc85f378b63083e87b8da0bc5e08d
yusakuhamajima <yusakuhamajima@gmail.com>
2020-02-03 21:50:46 +0900

    text

--- p5/canvas-size.js
+++ p5/canvas-size.js
@@ -2,11 +2,13 @@ function setup() {
   var canvas = createCanvas(320, 480, P2D);
   canvas.parent("canvas");
   colorMode(HSB, 360, 100, 100, 100);
+  textAlign(CENTER, CENTER);
 }
 
 function draw() {
   background(270, 100, 100, 100);
   noStroke();
   fill(90, 100, 100, 100);
-  rect(100, 100, 100, 100);
+  textSize(48);
+  text("320×480", width / 2, height / 2);
 }
