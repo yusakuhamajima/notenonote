547ed16c5736ca76aec0ebdc029e4457f0d6e013
yusakuhamajima <yusakuhamajima@gmail.com>
2020-03-12 22:11:42 +0900

    20200312

---
+++ js/nav.js
@@ -0,0 +1,7 @@
+document.write('<ul>');
+document.write('<li><a href="./index.html">top</a></li>');
+document.write('<li><a href="./visual.html">visual</a></li>');
+document.write('<li><a href="./playlist.html">playlist</a></li>');
+// document.write('<li><a href="./programming.html">programming</a></li>');
+// document.write('<li><a href="./design.html">design</a></li>');
+document.write('</ul>');
