0c6b80f91737b68c532e73eee82c92b005624bc7
yusakuhamajima <yusakuhamajima@gmail.com>
2020-03-15 16:02:47 +0900

    gtm

---
+++ js/weekly-playlist-nav.js
@@ -0,0 +1,5 @@
+document.write('<ul>');
+document.write('<li><a href="./09weeks2020.html">09weeks</a></li>');
+document.write('<li><a href="./10weeks2020.html">10weeks</a></li>');
+document.write('<li><a href="./11weeks2020.html">11weeks</a></li>');
+document.write('</ul>');
