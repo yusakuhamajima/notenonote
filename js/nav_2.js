a8d167e2ca18522df3d0601818db8f992843fc77
yusakuhamajima <yusakuhamajima@gmail.com>
2020-04-04 21:20:47 +0900

    nav

--- js/nav_2.js
+++ js/nav_2.js
@@ -2,7 +2,4 @@ document.write("<ul>");
 document.write('<li><a href="../index.html">top</a></li>');
 document.write('<li><a href="../cc/index.html">creative coding</a></li>');
 document.write('<li><a href="../playlist/index.html">playlist</a></li>');
-document.write('<li><a href="../design/index.html">design</a></li>');
-document.write('<li><a href="../programming/index.html">programming</a></li>');
-
 document.write("</ul>");
