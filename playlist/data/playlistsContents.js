0a2608457d8d2a0bca5654c0e03a7ac5dbd3b5b4
yusakuhamajima <yusakuhamajima@gmail.com>
2020-06-03 20:35:04 +0900

    playlist

---
+++ playlist/data/playlistsContents.js
@@ -0,0 +1,112 @@
+var playlistsContents = [
+	{
+		weeks: 1,
+		url: 'https://note.com/yusaku/n/n8ae1d58e84a8',
+		img: '../../images/21weeks2020.png',
+	},
+	{
+		weeks: 2,
+		url: 'https://note.com/yusaku/n/n8ae1d58e84a8',
+		img: '../../images/21weeks2020.png',
+	},
+	{
+		weeks: 3,
+		url: 'https://note.com/yusaku/n/n8ae1d58e84a8',
+		img: '../../images/21weeks2020.png',
+	},
+	{
+		weeks: 4,
+		url: 'https://note.com/yusaku/n/n8ae1d58e84a8',
+		img: '../../images/21weeks2020.png',
+	},
+	{
+		weeks: 5,
+		url: 'https://note.com/yusaku/n/n8ae1d58e84a8',
+		img: '../../images/21weeks2020.png',
+	},
+	{
+		weeks: 6,
+		url: 'https://note.com/yusaku/n/n8ae1d58e84a8',
+		img: '../../images/21weeks2020.png',
+	},
+	{
+		weeks: 7,
+		url: 'https://note.com/yusaku/n/n8ae1d58e84a8',
+		img: '../../images/21weeks2020.png',
+	},
+	{
+		weeks: 8,
+		url: 'https://note.com/yusaku/n/n8ae1d58e84a8',
+		img: '../../images/21weeks2020.png',
+	},
+	{
+		weeks: 9,
+		url: 'https://note.com/yusaku/n/n8ae1d58e84a8',
+		img: '../../images/21weeks2020.png',
+	},
+	{
+		weeks: 10,
+		url: './2020/10weeks2020.html',
+		img: '../../images/10weeks2020.png',
+	},
+	{
+		weeks: 11,
+		url: './2020/11weeks2020.html',
+		img: '../../images/11weeks2020.png',
+	},
+	{
+		weeks: 12,
+		url: './2020/12weeks2020.html',
+		img: '../../images/12weeks2020.png',
+	},
+	{
+		weeks: 13,
+		url: './2020/13weeks2020.html',
+		img: '../../images/13weeks2020.png',
+	},
+	{
+		weeks: 14,
+		url: './2020/14weeks2020.html',
+		img: '../../images/14weeks2020.png',
+	},
+	{
+		weeks: 15,
+		url: './2020/15weeks2020.html',
+		img: '../../images/15weeks2020.png',
+	},
+	{
+		weeks: 16,
+		url: './2020/16weeks2020.html',
+		img: '../../images/16weeks2020.png',
+	},
+	{
+		weeks: 17,
+		url: './2020/17weeks2020.html',
+		img: '../../images/17weeks2020.png',
+	},
+	{
+		weeks: 18,
+		url: './2020/18weeks2020.html',
+		img: '../../images/18weeks2020.png',
+	},
+	{
+		weeks: 19,
+		url: './2020/19weeks2020.html',
+		img: '../../images/19weeks2020.png',
+	},
+	{
+		weeks: 20,
+		url: './2020/20weeks2020.html',
+		img: '../../images/20weeks2020.png',
+	},
+	{
+		weeks: 21,
+		url: './2020/21weeks2020.html',
+		img: '../../images/21weeks2020.png',
+	},
+	{
+		weeks: 22,
+		url: './2020/22weeks2020.html',
+		img: '../../images/22weeks2020.png',
+	},
+];
