7af37c34167a50b5692a3d193eaf6aeb49543209
yusakuhamajima <yusakuhamajima@gmail.com>
2020-06-13 21:49:39 +0900

    waa-001

--- p5js_and_processing/p5js/script/p5js-001.js
+++ p5js_and_processing/p5js/script/p5js-001.js
@@ -1,6 +1,8 @@
 var w_canvas, h_canvas;
 w_canvas = 640;
 h_canvas = 335;
+var x = 0;
+var d = -1;
 
 function setup() {
 	createCanvas(w_canvas, h_canvas);
@@ -8,8 +10,11 @@ function setup() {
 
 function draw() {
 	background(220);
-	ellipse(50, 50, 80, 80);
-	ellipse(200, 50, 80, 80);
+	ellipse(x, 50, 80, 80);
+	x = x + 10 * d;
+	if (x < 0 || x > w_canvas) {
+		d = d * -1;
+	}
 }
 
 function keyPressed() {
